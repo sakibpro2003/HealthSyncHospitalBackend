3e8fb87d00d9321a07c0a3afba5be4d7db42beb9
sakibpro2003 <sakibprodhan2003@gmail.com>
2025-06-23 15:32:19 +0600

    refactor:refactored token access

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -1,17 +1,26 @@
-
 import catchAsync from "../../utils/catchAsync";
 import { sendResponse } from "../../utils/sendResponse";
 import { StatusCodes } from "http-status-codes";
 import { AuthServices } from "./auth.service";
 
 const loginUser = catchAsync(async (req, res) => {
-  const result = await AuthServices.loginUser(req.body);
+  const { accessToken, refreshToken } = await AuthServices.loginUser(req.body);
+
+  res.cookie("token", accessToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  });
+
   sendResponse(res, {
     statusCode: StatusCodes.OK,
     message: "Login successfull",
     success: true,
     data: {
-      result,
+      //!dont return tokens
+      // accessToken,
+      // refreshToken,
     },
   });
 });
